60a355475071c92e872961310d49e9b8779c8780
DeLightPlus <matlakalakabelo1@gmail.com>
2024-10-22 15:59:12 +0200

    Node.js Card Guessing Game

--- README.md
+++ README.md
@@ -1,31 +1,21 @@
-# Introduction To Node.js
-### Express.js
-Create a simple API  for a book Directory
-#### Requirements:
-* Book title
-* Author,
-* Publisher
-* Published date
-* ISBN(international standard book number)
-
-* ##### Managing Endpoints:
-    - Use the built-in http module to manage different HTTP methods (GET, POST, PUT/PATCH, DELETE).
-* ##### Handling Data Exchange:
-    - Handle JSON data exchange
-    - Implement functionality to parse and respond with JSON data.
-* ##### Basic Error Handling:
-    - Make sure you have validation.
-* ##### Objective:
-    - The objective of this project is to test trainees' understanding of 
-        Express, HTTP methods and endpoint creation, and how to handle data sent to  endpoints.
-
-##### Express - Node.js web application framework
-   * expressjs.com 
-
-
-
-   ```bash
-        curl -X PUT 
-        -H "Content-Type: application/json" 
-        -d '{"title": "Updated Book Title", "author": "Updated Author"}' 
-        http://localhost:8000/books/1
+https://ejs.co/
+Activity - Practice Set 5
+Trainees are required to plan, design and implement a card guessing game.
+Node.js Card Guessing Game:
+Objective:
+The objective of this project is to create a card guessing game using Node.js.  Users should be able to select two cards at a time, if they match they remain open, if they don’t match they flip to hide their contents again. The game is won when all cards are successfully matched to their corresponding cards.
+Interface:
+Design a user-friendly interface that is intuitive for the game
+Include a total of 36 cards: 6 rows and 6 columns. This will bring the matching set to 18
+When the game starts, the cards should be set dynamically and remain in their selected positions until the game ends or is restarted by the user
+Features:
+When the page is opened, automatically set the cards and hide their contents.
+Users can reset the game whenever they choose, changing the position of the cards
+When the user wins, display a pop up to congratulate the user on winning.
+Persistence:
+No data is persisted
+Testing:
+Test the application thoroughly to ensure that all features work as expected
+ejs.coejs.co
+EJS -- Embedded JavaScript templates
+'E' is for 'effective'. EJS is a simple templating language that lets you generate HTML markup with plain JavaScript. No religiousness about how to organize things. No reinvention of iteration and control-flow. It's just plain JavaScript.
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 
 const fs = require('fs');
+const { title } = require("process");
 const filePath = './books.json';
 
 app.use(express.json());
@@ -16,120 +17,24 @@ if (!fs.existsSync(filePath))
 // Read books from file
 let books = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>
     {        
-        res.json(books);             
-        // res.send("Hello, World!");         
+        // res.json(books);             
+        res.render("index", {title:'Home'});         
     });
 
-app.get('/books', (req, res)=>
+app.get('/about', (req, res)=>
     {        
-        res.json(books);
-        // res.send("Hello, World!");
+            // res.json(books);             
+        res.render("about");         
     });
 
-app.get('/books/:id', (req, res)=>
-    {
-        const id = req.params.id;       
-        // res.send(`about/id=${id} /Hello, World!`);
-
-        const jsonData = fs.readFileSync('books.json');
-        const books = JSON.parse(jsonData);
-        const book = books.find((book) => book.id == id);
-
-        if (book) { res.send(book); }
-        else { res.status(404).send({ message: 'Book not found' }); }
-    });
-
-app.get('/books/isbn/:isbn', (req, res) =>
-    {
-        const isbn = req.params.isbn;
-        const jsonData = fs.readFileSync('books.json');
-        const books = JSON.parse(jsonData);
-        const book = books.find((book) => book.isbn == isbn);
-
-        if(book){ res.send(book); }
-        else { res.status(404).send({ message: 'Book with that number is not found' }); }
-    });
-
-/////////////////////////////////////
-
-// POST /books - Create a new book
-app.post('/books', (req, res) => 
-    {
-        console.log(req.body);
-    
-        const { title, author, publisher, publishedDate, isbn } = req.body;
-    
-        if (!title || !author || !publisher || !publishedDate || !isbn) 
-        {
-            return res.status(400).json({ error: 'Missing required fields' });
-        }
-    
-        const newBook = { id: books.length+1, title, author, publisher, publishedDate, isbn };
-        books.push(newBook);
-        fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
-        // fs.writeFileSync(filePath, books);
-        res.json(newBook);
-    });
-
-// DELETE /books/:id - Delete a book by ID
-app.delete('/books/:id', (req, res) => 
-    {
-        const id = parseInt(req.params.id); // Convert the id to a number
-        const jsonData = fs.readFileSync(filePath);
-        let books = JSON.parse(jsonData);
-        // console.log(books);    
-        
-        const index = books.findIndex((book) => book.id === id);
-        // console.log(index);
-        if (index !== -1) 
-        {
-            books.splice(index, 1);
-            // Write the updated array back to the JSON file
-            fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
-            res.status(200).send({ message: 'Book deleted successfully' });
-        } else {  res.status(404).send({ message: 'Book not found' }); }
-    
-    });
-
-// PUT /books/:id - Update a book by ID
-app.put('/books/:id', (req, res) => 
-    {
-        const id = parseInt(req.params.id); // Convert the id to a number
-        const jsonData = fs.readFileSync(filePath);
-        let books = JSON.parse(jsonData);
-
-        const index = books.findIndex((book) => book.id === id);
-
-        if (index !== -1) 
-        {
-            // console.log(req.body);        
-            // If the book is found, update its properties
-            const updatedBook = { ...books[index], ...req.body };
-            books[index] = updatedBook;
-
-            console.log( updatedBook);       
-
-            // Write the updated array back to the JSON file
-            fs.writeFileSync(filePath, JSON.stringify(books, null, 2));
-            res.json(updatedBook);
-        } 
-        else { res.status(404).send({ message: 'Book not found' }); }
-    });  
-
-
-app.patch('/books/:id', (req, res) => 
-    {
-        const id = req.params.id;
-        const book = books.find((book) => book.id === parseInt(id));
-        
-        if (!book) { return res.status(404).send({ message: 'Book not found' }); }
-        
-        const updatedBook = { ...book, ...req.body };
-        books[books.indexOf(book)] = updatedBook;
-        
-        res.json(updatedBook);
+app.get('/404', (req, res)=>
+    {        
+        // res.json(books);             
+        res.render("404");         
     });
 
 
