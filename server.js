bdee33fc93daa57eb45b3f3ae999ae1ec3e7a30d
DeLightPlus <matlakalakabelo1@gmail.com>
2024-10-25 14:11:21 +0200

    update Guessing Game

--- server.js
+++ server.js
@@ -2,24 +2,36 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-// app.use(express.json());
-
 app.set('view engine', 'ejs');
 
+
+// console.log(__dirname);
+app.use(express.static('public'));
+// app.use(express.json());
+
+const cards = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", 
+    "0", "0", "A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H" ];
+
+
+    
+// Function to shuffle cards
+function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+// Shuffle cards
+var random_cards = shuffle([...cards]);
+
 app.get('/', (req, res)=>
-    {        
+    {           
+  
         // res.json(books);             
-        res.render("index", { title:'Home' });         
+        res.render("index", { title:'Home', random_cards });         
     });
 
-app.get('/404', (req, res)=>
-    {        
-        // res.json(books);             
-        res.render("404");         
+app.get('*', (req, res) => {
+        res.status(404).render('404', { title: '404' });
     });
 
 
 app.listen(port, () => { console.log(`Server started on port ${port}`);  });
-
-// curl -X PUT -H "Content-Type: application/json" -d '{"title": "Updated Book Title", "author": "Updated Author"}' http://localhost:8000/books/1
-// curl -X POST -H "Content-Type": "application/json" -d '{"title": "New Book 2", "author": "New Author 2",  "publisher": "New Publisher 2", "publishedDate": "2023-11-19","isbn": "1234567890128"}' http://localhost:8000/books
